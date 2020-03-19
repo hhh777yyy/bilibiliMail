88c76b50e39e83987189d3dd64c6bafdfab4cedd
hhh777yyy <1119848245@qq.com>
2020-03-19 23:06:54 +0800

    v1.1 增加log4j日志，发送邮箱服务器由163邮箱变为使用qq邮箱

--- src/main/java/util/HttpClient.java
+++ src/main/java/util/HttpClient.java
@@ -1,8 +1,6 @@
 package util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -31,18 +29,23 @@ public class HttpClient {
     }
 
 
-    public String client(String urlString,Integer mid,String pdata) throws MalformedURLException {
+    public String client(String urlString,Integer mid,String pdata)   throws MalformedURLException{
         StringBuilder json = new StringBuilder();
         URL urlObject = new URL(urlString+mid+pdata);
         try {
+
 //            URL urlObject = new URL(url);
             URLConnection uc = urlObject.openConnection();
+
+//            System.out.println(uc.getConnectTimeout());
+
             // 设置为utf-8的编码 解决中文乱码
             BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream(), "utf-8"));
             String inputLine = null;
             while ((inputLine = in.readLine()) != null) {
                 json.append(inputLine);
             }
+
             in.close();
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -53,3 +56,7 @@ public class HttpClient {
     }
 
 }
+
+
+
+
