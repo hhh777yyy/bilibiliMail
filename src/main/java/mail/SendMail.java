240cc5fb0bb679178aed9266de6199d3531c7ee1
hhh777yyy <1119848245@qq.com>
2020-03-22 18:41:38 +0800

    v1.2 优化代码

--- src/main/java/mail/SendMail.java
+++ src/main/java/mail/SendMail.java
@@ -11,24 +11,21 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 
 public class SendMail {
-    static Logger logger = Logger.getLogger(SendMail.class.getName());
+    private static Logger logger = Logger.getLogger(SendMail.class.getName());
 
-    public static String myEmailAccount = "1119848245@qq.com";//邮箱账号
-    public static String myEmailPassword = "omvhcosjewojhbig";//邮箱密码
-    public static String myEmailSMTPHost = "smtp.qq.com";
+    private static String myEmailAccount = "1119848245@qq.com";//邮箱账号
+    private static String myEmailPassword = "omvhcosjewojhbig";//邮箱密码
+    private static String myEmailSMTPHost = "smtp.qq.com";
 //    发送到指定的单个邮箱
-    public static Boolean sendEmail(String text,String receiveEmail) throws MessagingException {
-        Boolean sendOrNot = false ;
-        Transport transport = null;
+    public static Boolean sendEmail(String text,String receiveEmail){
+        boolean sendOrNot = false ;
+        Transport transport;
 
         try {
-
             Properties props = new Properties();
             props.setProperty("mail.transport.protocol", "smtp");
             props.setProperty("mail.smtp.host", myEmailSMTPHost);
@@ -40,7 +37,7 @@ public class SendMail {
             props.setProperty("mail.smtp.socketFactory.port", smtpPort);
             Session session = Session.getDefaultInstance(props);
                                // 设置为debug模式, 可以查看详细的发送 log
-            session.setDebugOut(new PrintStream(new  LoggingOutStream(Logger.getRootLogger(), Level.ERROR), true));
+            session.setDebugOut(new PrintStream(new  LoggingOutStream(Logger.getRootLogger(), Level.DEBUG), true));
             session.setDebug(true);
             MimeMessage message = createMimeMessage(session, myEmailAccount, receiveEmail,text);
             transport = session.getTransport();
@@ -51,17 +48,14 @@ public class SendMail {
         } catch (MessagingException e) {
             e.printStackTrace();
             logger.error(e);
-
         } catch (Exception e) {
             e.printStackTrace();
-            logger.error(e);
-
         }
         return sendOrNot;
     }
 
 
-    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail,String text) throws Exception {
+    private static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail, String text) throws Exception {
         MimeMessage message = new MimeMessage(session);
         message.setFrom(new InternetAddress(sendMail, "1119848245", "UTF-8"));
         //new InternetAddress中personal属性为邮件的收/发件人的名字
@@ -74,53 +68,9 @@ public class SendMail {
     }
 
 
-//    public static void main(String[] args) throws Exception {
-//        List<String> list= Arrays.asList("15626128581@163.com","1119848245@qq.com");
-//        sendEmail2("这是内容，验证码45845",list);
-//        Boolean sendEmail = sendEmail("这是内容，验证码45845", "15626128581@163.com");
-//        System.out.println(sendEmail);
-//    }
-
-
-
-
-
-
-
-
-
-//    发送到多个邮箱
-//    public static void sendEmail2(String text, List<String> receiveEmail) throws Exception {
-//        Properties props = new Properties();
-//        MimeMessage message = null;
-//        props.setProperty("mail.transport.protocol", "smtp");
-//        props.setProperty("mail.smtp.host", myEmailSMTPHost);
-//        props.setProperty("mail.smtp.auth", "true");
-//        final String smtpPort = "465";
-//        props.setProperty("mail.smtp.port", smtpPort);
-//        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//        props.setProperty("mail.smtp.socketFactory.fallback", "false");
-//        props.setProperty("mail.smtp.socketFactory.port", smtpPort);
-//        Session session = Session.getDefaultInstance(props);
-//        session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
-//        for (int i=0;i<receiveEmail.size();i++){
-//
-//            message = createMimeMessage(session, myEmailAccount, receiveEmail.get(i),text);
-//
-//
-//            Transport transport = session.getTransport();
-//            transport.connect(myEmailAccount, myEmailPassword);
-//            transport.sendMessage(message, message.getAllRecipients());
-//        }
-////        transport.close();
-//    }
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        Boolean sendEmail = sendEmail("这是内容，验证码45845", "15626128581@163.com");
+        System.out.println(sendEmail);
+    }
 
 }
